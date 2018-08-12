794869d7decdab309e3a199677ce1d149bc664cb
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2018-08-12 19:21:55 +0530

    Add variable name support

--- interpreter.py
+++ interpreter.py
@@ -6,6 +6,7 @@ class Interpreter(object):
 	
 	def __init__(self):
 		self.stack = []
+		self.environment = {}
 	
 	def LOAD_VALUE(self, number):
 		self.stack.append(number)
@@ -14,34 +15,58 @@ class Interpreter(object):
 		answer = self.stack.pop()
 		print(answer)
 
+	def STORE_NAME(self, name):
+		value = self.stack.pop()
+		self.environment[name] = value
+
+	def LOAD_NAME(self, name):
+		value = self.environment.get(name)
+		self.stack.append(value)
+
 	def ADD_VALUES(self):
 		second_number = self.stack.pop()
 		first_number = self.stack.pop()
 		answer = first_number + second_number
 		self.stack.append(answer)
 
+	def parse_arguments(self, intructions, argument, what_to_execute):
+		names = ["LOAD_NAME", "STORE_NAME"]
+		numbers = ["LOAD_VALUE"]
+
+
+		if intructions in names:
+			argument = what_to_execute['names'][argument]
+		elif intructions in numbers:
+			argument = what_to_execute['numbers'][argument]
+
+		return argument
+
 	def run_code(self, what_to_execute):
 		instructions = what_to_execute['instructions']
 		numbers = what_to_execute['numbers']
-		for instruction in instructions:
-			event, index = instruction
-			if event == 'LOAD_VALUE':
-				number = numbers[index]
-				self.LOAD_VALUE(number)
-			elif event == 'ADD_VALUES':
-				self.ADD_VALUES()
-			elif event == 'PRINT_ANSWER':
-				self.PRINT_ANSWER()
+		names = what_to_execute['names']
 
+		for instruction in instructions:
+			#import pdb;pdb.set_trace()
+			event, index = instruction
+			arg = self.parse_arguments(event, index, what_to_execute)
+			bytecode_method = getattr(self, event)
+			if arg is None:
+				bytecode_method()
+			else:
+				bytecode_method(arg)
 
 what_to_execute = {
         "instructions": [("LOAD_VALUE", 0),
+                         ("STORE_NAME", 0),
                          ("LOAD_VALUE", 1),
-                         ("ADD_TWO_VALUES", None),
-                         ("LOAD_VALUE", 2),
+                         ("STORE_NAME", 1),
+                         ("LOAD_NAME", 0),
+                         ("LOAD_NAME", 1),
                          ("ADD_VALUES", None),
                          ("PRINT_ANSWER", None)],
-        "numbers": [7, 5, 8]
+        "numbers": [1, 2],
+        "names":   ["a", "b"]
 }
 
 interpreter = Interpreter()
